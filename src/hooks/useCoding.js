359a5303e554c8c1bcbdf4747f8bdee9843abf93
sunyue <sunyue@knowhy.work>
2024-09-14 23:47:30 +0800

    fix(code): Modify the encoding method of the code

--- src/hooks/useCoding.js
+++ src/hooks/useCoding.js
@@ -2,10 +2,11 @@ import useConsole from "@/hooks/useConsole"
 
 export default function useCoding(fileRaw) {
     const [results, setResults] = useState(["The results will be displayed here."])
-    const [code, setCode] = useState(`const runTest = foo()\nconsole.log(runTest)`)
+    const [code, setCode] = useState(`const code = 'return (leftcode)'\nconst fn = new Function(code);\nconst runTest = fn();\nrunTest('111');
+`)
 
     function handleExecute() {
-        const runCode = fileRaw + "\n" + code
+        const runCode = fileRaw + "\n" + code.replace(/leftcode/g, String(fileRaw.replace(/\n/g, '')))
         const fn = new Function(runCode)
         try {
             const result = useConsole(fn)
